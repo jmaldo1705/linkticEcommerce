b3985c68b67d42b3c82df91023c9c6def8619446
Jonathan Maldonado <jmmaldon@bancolombia.com.co>
2024-09-10 15:17:37 -0500

    first commit

---
+++ src/main/java/com/linktic/ecommerce/LinkticEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkticEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkticEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linktic/ecommerce/LinkticEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkticEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
